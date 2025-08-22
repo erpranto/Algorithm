a34382ea2039c1eebd72f86878f768abcaa9b823
erpranto <erpranto55@gmail.com>
2025-08-22 20:08:06 +0600

    Assignment 1 Done

--- Week_1-Graph_Traversal_Algorithm/Assignment_1/Count_Apartments_II.cpp
+++ Week_1-Graph_Traversal_Algorithm/Assignment_1/Count_Apartments_II.cpp
@@ -1,7 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
+char grid[1005][1005];
+bool vis[1005][1005];
+vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, m;
+vector<int> apartments;
+
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+        return false;
+    return true;
+}
+int dfs(int si, int sj)
+{
+    vis[si][sj] = true;
+    int cnt = 1;
+    for (int i = 0; i < d.size(); i++)
+    {
+        int ci, cj;
+        ci = si + d[i].first;
+        cj = sj + d[i].second;
+        if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.')
+            cnt += dfs(ci, cj);
+    }
+    return cnt;
+}
 int main()
 {
-    
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> grid[i][j];
+    memset(vis, false, sizeof(vis));
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (!vis[i][j] && grid[i][j] == '.')
+            {
+                int sz=dfs(i, j);
+                apartments.push_back(sz);
+            }
+        }
+    }
+    if(apartments.empty())
+        cout<<0<<endl;
+    else
+        {
+            sort(apartments.begin(),apartments.end());
+            for(int i:apartments)
+                cout<<i<<" ";
+            
+        }
     return 0;
 }
